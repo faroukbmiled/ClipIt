08ac128e167fe31992aab6b86a3c049ebb0c21c4
mahdixabid <mahdiabid158@gmail.com>
2024-01-07 02:16:34 +0100

    adding user info card on hover

--- components/HomeBlocks/most_followed_users.jsx
+++ components/HomeBlocks/most_followed_users.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import followers_icon from "../../src/assets/Icons/followers_icon.svg";
+import eye_icon from "../../src/assets/Icons/eye.svg";
+import videos_icon from "../../src/assets/Icons/videos.svg";
 import "swiper/css";
 import Link from 'next/link';
 
@@ -30,13 +32,24 @@ export default function MostFollowedUsers() {
           {mostFollowedUsers.length === 0 ? (
             <p>Loading...</p>
           ) : (
-            <Swiper slidesPerView={4}>
+            <Swiper slidesPerView={4} spaceBetween={20}>
               {mostFollowedUsers.map((user) => (
                 <SwiperSlide key={user.userId}>
                   <div className="card-user fl_col gp10">
                     <Link href={"/user/" + user.userId}></Link>
                     <div className="user-avatar">
-                      <img className="rd20" src={user.avatar} alt="" />
+                      <img className="rd20 user-img" src={user.avatar} alt="" />
+                      <div className="user-info pd40 rd20 fl_col txt_white">
+                        <p className="p24 ">{user.name}</p>
+                        <div className="fl_row gp5 ai_c">
+                          <img src={eye_icon.src} alt="" />
+                          <p className="p22">15 Videos</p>
+                        </div>
+                        <div className="fl_row gp5 ai_c">
+                          <img src={videos_icon.src} alt="" />
+                          <p className="p22">150 Views</p>
+                        </div>
+                      </div>
                     </div>
                     <div className="fl_row gp10 jc_c">
                       <img src={followers_icon.src} alt="" />
@@ -45,6 +58,7 @@ export default function MostFollowedUsers() {
                       </p>
                     </div>
                   </div>
+
                 </SwiperSlide>
               ))}
             </Swiper>
