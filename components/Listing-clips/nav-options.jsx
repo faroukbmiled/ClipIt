ca0e76462fdf0b8a5abd9d0b6bbcf840ee14ebd2
mahdixabid <mahdiabid158@gmail.com>
2024-01-10 22:45:42 +0100

    updating nav links

--- components/Listing-clips/nav-options.jsx
+++ components/Listing-clips/nav-options.jsx
@@ -11,17 +11,17 @@ function NavOptions() {
                 <ul className="menu-list fl_col gp5">
                     <li className="fl_row ai_c gp24 txt_white pd10-t-b pd28-r-l p14">
                         <img src={homeIcon.src} alt="homeIcon" />
-                        <Link href="/Home"></Link>
+                        <Link href="/"></Link>
                         <p className="p14">Home</p>
                     </li>
                     <li className="fl_row ai_c gp24 txt_white pd10-t-b pd28-r-l p14">
                         <img src={YourvideosIcon.src} alt="YourvideosIcon" />
-                        <Link href="/Your-Videos"></Link>
+                        <Link href="/user/me?tab=videos"></Link>
                         <p className="p14">Your Videos</p>
                     </li>
                     <li className="fl_row ai_c gp24 txt_white pd10-t-b pd28-r-l p14">
                         <img src={SubscriptionsIcon.src} alt="SubscriptionsIcon" />
-                        <Link href="/Subscriptions"></Link>
+                        <Link href="/user/me?tab=following"></Link>
                         <p className="p14">Subscriptions</p>
                     </li>
                     <li className="fl_row ai_c gp24 txt_white pd10-t-b pd28-r-l p14">
